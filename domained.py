73a691e00e9a6bef13267ddf751d6a5a7cfcff17
Caleb Kinney <cakinney@gmail.com>
2018-11-13 04:54:57 -0500

    Black formatted
    https://github.com/ambv/black

--- domained.py
+++ domained.py
@@ -31,51 +31,64 @@ today = datetime.date.today()
 
 
 def get_args():
-    parser = argparse.ArgumentParser(
-        description='domained')
+    parser = argparse.ArgumentParser(description="domained")
     parser.add_argument(
-        '-d', '--domain', type=str, help='Domain', required=False, default=False)
+        "-d", "--domain", type=str, help="Domain", required=False, default=False
+    )
     parser.add_argument(
-        '-s', '--secure', help='Secure', action='store_true', required=False, default=False)
+        "-s",
+        "--secure",
+        help="Secure",
+        action="store_true",
+        required=False,
+        default=False,
+    )
     parser.add_argument(
-        '-b', '--bruteforce', help='Bruceforce', action='store_true', default=False)
+        "-b", "--bruteforce", help="Bruceforce", action="store_true", default=False
+    )
+    parser.add_argument("--upgrade", help="Upgrade", action="store_true", default=False)
+    parser.add_argument("--install", help="Install", action="store_true", default=False)
+    parser.add_argument("--vpn", help="VPN Check", action="store_true", default=False)
     parser.add_argument(
-        '--upgrade', help='Upgrade', action='store_true', default=False)
+        "-p", "--ports", help="Ports", action="store_true", default=False
+    )
     parser.add_argument(
-        '--install', help='Install', action='store_true', default=False)
+        "-q", "--quick", help="Quick", action="store_true", default=False
+    )
     parser.add_argument(
-        '--vpn', help='VPN Check', action='store_true', default=False)
+        "--bruteall", help="Bruteforce JHaddix All", action="store_true", default=False
+    )
     parser.add_argument(
-        '-p', '--ports', help='Ports', action='store_true', default=False)
+        "--fresh", help="Remove output Folder", action="store_true", default=False
+    )
     parser.add_argument(
-        '-q', '--quick', help='Quick', action='store_true', default=False)
+        "--notify", help="Notify when script completed", nargs="?", default=False
+    )
     parser.add_argument(
-        '--bruteall', help='Bruteforce JHaddix All', action='store_true', default=False)
+        "--active", help="EyeWitness Active Scan", action="store_true", default=False
+    )
     parser.add_argument(
-        '--fresh', help='Remove output Folder', action='store_true', default=False)
-    parser.add_argument(
-        '--notify', help='Notify when script completed', nargs='?', default=False)
-    parser.add_argument(
-        '--active', help='EyeWitness Active Scan', action='store_true', default=False)
-    parser.add_argument(
-        '--noeyewitness', help='No EyeWitness', action='store_true', default=False)
+        "--noeyewitness", help="No EyeWitness", action="store_true", default=False
+    )
 
     return parser.parse_args()
 
 
-newpath = r'output'
+newpath = r"output"
 if not os.path.exists(newpath):
     os.makedirs(newpath)
 
 
 def banner():
-    print("""\033[1;31m
+    print(
+        """\033[1;31m
          ___/ /__  __ _  ___ _(_)__  ___ ___/ /
         / _  / _ \/  ' \/ _ `/ / _ \/ -_) _  /
         \_,_/\___/_/_/_/\_,_/_/_//_/\__/\_,_/
-    \033[1;34m\t\t\tgithub.com/cakinney\033[1;m""")
-    globpath = ("*.csv")
-    globpath2 = ("*.lst")
+    \033[1;34m\t\t\tgithub.com/cakinney\033[1;m"""
+    )
+    globpath = "*.csv"
+    globpath2 = "*.lst"
     if (next(glob.iglob(globpath), None)) or (next(glob.iglob(globpath2), None)):
         print("\nThe following files may be left over from failed domained attempts:")
         for file in glob.glob(globpath):
@@ -102,8 +115,11 @@ def sublist3r(brute=False):
     print("\n\n\033[1;31mRunning Sublist3r \n\033[1;37m")
     sublist3rFileName = "{}_sublist3r.txt".format(output_base)
     Subcmd = "python {} -v -t 15 {} -d {} -o {}".format(
-        os.path.join(script_path, 'bin/Sublist3r/sublist3r.py'),
-        '-b' if brute else '', domain, sublist3rFileName)
+        os.path.join(script_path, "bin/Sublist3r/sublist3r.py"),
+        "-b" if brute else "",
+        domain,
+        sublist3rFileName,
+    )
     print("\n\033[1;31mRunning Command: \033[1;37m{}".format(Subcmd))
     os.system(Subcmd)
     print("\n\033[1;31mSublist3r Complete\033[1;37m")
@@ -115,7 +131,8 @@ def sublist3r(brute=False):
 def enumall():
     print("\n\n\033[1;31mRunning Enumall \n\033[1;37m")
     enumallCMD = "python {} {}".format(
-        os.path.join(script_path, 'bin/domain/enumall.py'), domain)
+        os.path.join(script_path, "bin/domain/enumall.py"), domain
+    )
     print("\n\033[1;31mRunning Command: \033[1;37m{}".format(enumallCMD))
     os.system(enumallCMD)
     print("\n\033[1;31menumall Complete\033[1;37m")
@@ -124,10 +141,16 @@ def enumall():
 
 def massdns():
     print("\n\n\033[1;31mRunning massdns \n\033[1;37m")
-    word_file = os.path.join(script_path, 'bin/sublst/all.txt' if bruteall else 'bin/sublst/sl-domains.txt')
-    massdnsCMD = 'python {} -s {} {} | {} -r resolvers.txt -t A -a -o -w {}-massdns.txt -'.format(
-        os.path.join(script_path, 'bin/subbrute/subbrute.py'), word_file, domain,
-        os.path.join(script_path, 'bin/massdns/bin/massdns'), output_base)
+    word_file = os.path.join(
+        script_path, "bin/sublst/all.txt" if bruteall else "bin/sublst/sl-domains.txt"
+    )
+    massdnsCMD = "python {} -s {} {} | {} -r resolvers.txt -t A -a -o -w {}-massdns.txt -".format(
+        os.path.join(script_path, "bin/subbrute/subbrute.py"),
+        word_file,
+        domain,
+        os.path.join(script_path, "bin/massdns/bin/massdns"),
+        output_base,
+    )
     print("\n\033[1;31mRunning Command: \033[1;37m{}".format(massdnsCMD))
     os.system(massdnsCMD)
     print("\n\033[1;31mMasscan Complete\033[1;37m")
@@ -137,7 +160,8 @@ def massdns():
 def knockpy():
     print("\n\n\033[1;31mRunning Knock \n\033[1;37m")
     knockpyCmd = "python {} -c {}".format(
-        os.path.join(script_path, 'bin/knockpy/knockpy/knockpy.py'), domain)
+        os.path.join(script_path, "bin/knockpy/knockpy/knockpy.py"), domain
+    )
     print("\n\033[1;31mRunning Command: \033[1;37m {}".format(knockpyCmd))
     os.system(knockpyCmd)
     rootdomainStrip = domain.replace(".", "_")
@@ -146,8 +170,8 @@ def knockpy():
     time.sleep(1)
     knockpySubs = []
     try:
-        with open(knockpyFilenameInit, 'rb') as f:
-            reader = csv.reader(f, delimiter=',')
+        with open(knockpyFilenameInit, "rb") as f:
+            reader = csv.reader(f, delimiter=",")
             for row in reader:
                 knockpySubs.append(row[3])
         filenameKnocktxt = "{}.txt".format(knockpyFilenameInit)
@@ -184,13 +208,18 @@ def subfinder():
 def eyewitness(filename):
     print("\n\n\033[1;31mRunning EyeWitness  \n\033[1;37m")
     EWHTTPScriptIPS = "python {} -f {} {} --no-prompt --headless  -d {}-{}-EW".format(
-        os.path.join(script_path, 'bin/EyeWitness/EyeWitness.py'), filename,
-        '--active-scan' if active else '',
-        output_base, time.strftime('%m-%d-%y-%H-%M'))
+        os.path.join(script_path, "bin/EyeWitness/EyeWitness.py"),
+        filename,
+        "--active-scan" if active else "",
+        output_base,
+        time.strftime("%m-%d-%y-%H-%M"),
+    )
     if vpn:
         print(
             "\n\033[1;31mIf not connected to VPN manually run the following command on reconnect:\n\033[1;37m{}".format(
-                EWHTTPScriptIPS))
+                EWHTTPScriptIPS
+            )
+        )
         vpncheck()
     print("\n\033[1;31mRunning Command: \033[1;37m{}".format(EWHTTPScriptIPS))
     os.system(EWHTTPScriptIPS)
@@ -198,80 +227,81 @@ def eyewitness(filename):
 
 
 def upgradeFiles():
-    binpath = os.path.join(script_path, 'bin')
+    binpath = os.path.join(script_path, "bin")
     old_wd = os.getcwd()
     if not os.path.exists(binpath):
         os.makedirs(binpath)
     else:
         print("Removing old bin directory: {}".format(binpath))
-        os.system('rm -rf {}'.format(binpath))
+        os.system("rm -rf {}".format(binpath))
         os.makedirs(binpath)
-    print('Changing into domained home: {}'.format(script_path))
+    print("Changing into domained home: {}".format(script_path))
     os.chdir(script_path)
-    unameChk = str(subprocess.check_output(['uname', '-am']))
+    unameChk = str(subprocess.check_output(["uname", "-am"]))
     if "kali" not in unameChk:
         print("\n\033[1;31mKali Linux Recommended!\033[1;37m")
         time.sleep(1)
-    sublist3rUpgrade = ("git clone https://github.com/aboul3la/Sublist3r.git ./bin/Sublist3r")
+    sublist3rUpgrade = (
+        "git clone https://github.com/aboul3la/Sublist3r.git ./bin/Sublist3r"
+    )
     print("\n\033[1;31mInstalling Sublist3r \033[1;37m")
     os.system(sublist3rUpgrade)
-    subInstallReq = ("pip install -r bin/Sublist3r/requirements.txt")
+    subInstallReq = "pip install -r bin/Sublist3r/requirements.txt"
     os.system(subInstallReq)
     print("Sublist3r Installed\n")
-    eyeWitnessUpgrade = ("git clone https://github.com/FortyNorthSecurity/EyeWitness.git ./bin/EyeWitness")
+    eyeWitnessUpgrade = "git clone https://github.com/FortyNorthSecurity/EyeWitness.git ./bin/EyeWitness"
     print("\n\033[1;31mInstalling EyeWitness \033[1;37m" + eyeWitnessUpgrade)
     os.system(eyeWitnessUpgrade)
-    eyeInstallReq = ("bash bin/EyeWitness/setup/setup.sh")
+    eyeInstallReq = "bash bin/EyeWitness/setup/setup.sh"
     print("\n\033[1;31mRunning Command: \033[1;37m")
     os.system(eyeInstallReq)
-    cpphantomjs = ("cp phantomjs ./bin/EyeWitness/bin/")
+    cpphantomjs = "cp phantomjs ./bin/EyeWitness/bin/"
     os.system(cpphantomjs)
-    movephantomjs = ("mv phantomjs bin/")
+    movephantomjs = "mv phantomjs bin/"
     os.system(movephantomjs)
     print("\nEyeWitness Installed\n")
-    enumallUpgrade = ("git clone https://github.com/jhaddix/domain.git ./bin/domain")
+    enumallUpgrade = "git clone https://github.com/jhaddix/domain.git ./bin/domain"
     print("\n\033[1;31mInstalling Enumall \033[1;37m")
     print("\nenumall Installed\n")
     os.system(enumallUpgrade)
-    knockpyUpgrade = ("git clone https://github.com/guelfoweb/knock.git ./bin/knockpy")
+    knockpyUpgrade = "git clone https://github.com/guelfoweb/knock.git ./bin/knockpy"
     print("\n\033[1;31mInstalling Knock \033[1;37m")
     os.system(knockpyUpgrade)
     print("\nKnockpy Installed\n")
-    sublstUpgrade = ("git clone https://gist.github.com/jhaddix/86a06c5dc309d08580a018c66354a056 ./bin/sublst")
+    sublstUpgrade = "git clone https://gist.github.com/jhaddix/86a06c5dc309d08580a018c66354a056 ./bin/sublst"
     print("\n\033[1;31mCopying JHaddix All Domain List: \033[1;37m")
     print("\nJHaddix All Domain List Installed\n")
     os.system(sublstUpgrade)
-    SLsublstUpgrade = (
-        "wget -O ./bin/sublst/sl-domains.txt https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/DNS/sortedcombied-knock-dnsrecon-fierce-reconng.txt")
+    SLsublstUpgrade = "wget -O ./bin/sublst/sl-domains.txt https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/DNS/sortedcombied-knock-dnsrecon-fierce-reconng.txt"
     print("\n\033[1;31mCopying SecList Domain List \033[1;37m")
     print("\nSecList Domain List Installed\n")
     os.system(SLsublstUpgrade)
-    subbruteUpgrade = ("git clone https://github.com/TheRook/subbrute.git ./bin/subbrute")
+    subbruteUpgrade = "git clone https://github.com/TheRook/subbrute.git ./bin/subbrute"
     print("\n\033[1;31mInstalling Subbrute \033[1;37m")
     os.system(subbruteUpgrade)
     print("\nSubbrute Installed\n")
-    amassUpgrade = ("go get -u github.com/OWASP/Amass/...")
+    amassUpgrade = "go get -u github.com/OWASP/Amass/..."
     print("\n\033[1;31mInstalling Amass \033[1;37m")
     os.system(amassUpgrade)
-    subfinderUpgrade = ("go get -u github.com/subfinder/subfinder")
+    subfinderUpgrade = "go get -u github.com/subfinder/subfinder"
     print("\n\033[1;31mInstalling Subfinder \033[1;37m")
     os.system(subfinderUpgrade)
-    massdnsUpgrade = ("git clone --branch v0.2 --single-branch https://github.com/blechschmidt/massdns ./bin/massdns")
+    massdnsUpgrade = "git clone --branch v0.2 --single-branch https://github.com/blechschmidt/massdns ./bin/massdns"
     print("\n\033[1;31mInstalling massdns \033[1;37m")
     os.system(massdnsUpgrade)
-    massdnsMake = ("make -C ./bin/massdns")
+    massdnsMake = "make -C ./bin/massdns"
     os.system(massdnsMake)
     print("\nMassdns Installed\n")
     os.system("cp ./bin/subbrute/resolvers.txt ./")
     if "kali" in unameChk:
-        reconNGInstall = ("apt-get install recon-ng")
+        reconNGInstall = "apt-get install recon-ng"
         print("\n\033[1;31mInstalling Recon-ng \033[1;37m")
         os.system(reconNGInstall)
         print("\nRecon-ng Installed\n")
     else:
         print("Please install Recon-ng - https://bitbucket.org/LaNMaSteR53/")
     print("\n\033[1;31mAll tools installed \033[1;37m")
-    print('Changing back to old working directory: {}'.format(old_wd))
+    print("Changing back to old working directory: {}".format(old_wd))
     os.chdir(old_wd)
 
 
@@ -318,7 +348,7 @@ def subdomainfile():
             subdomainCounter = subdomainCounter + 1
         f1.close()
         os.remove(enumallFileName)
-        enumallFileNamecsv = (domain + ".csv")
+        enumallFileNamecsv = domain + ".csv"
         os.remove(enumallFileNamecsv)
         print("\n{} Subdomains discovered by Enumall".format(subdomainCounter))
     except:
@@ -401,13 +431,13 @@ def subdomainfile():
     except:
         print("\nError Opening Subfinder File!\n")
     print("\nCombining Domains Lists\n")
-    domainList = open(subdomainAllFile, 'r')
+    domainList = open(subdomainAllFile, "r")
     uniqueDomains = set(domainList)
     domainList.close()
     subdomainUniqueFile = "{}-unique.txt".format(output_base)
-    uniqueDomainsOut = open(subdomainUniqueFile, 'w')
+    uniqueDomainsOut = open(subdomainUniqueFile, "w")
     for domains in uniqueDomains:
-        domains = domains.replace('\n', '')
+        domains = domains.replace("\n", "")
         if domains.endswith(domain):
             uniqueDomainsOut.writelines("https://{}\n".format(domains))
             if ports is not False:
@@ -430,7 +460,7 @@ def subdomainfile():
 
 
 def vpncheck():
-    vpnck = requests.get('https://ifconfig.co/json')
+    vpnck = requests.get("https://ifconfig.co/json")
     # Change "City" to your city")
     if "City" in vpnck.text:
         print("\n\033[1;31mNot connected via VPN \033[1;37m")
@@ -444,42 +474,57 @@ def vpncheck():
 
 
 def notified():
-    notifySub = ("domained Script Finished")
+    notifySub = "domained Script Finished"
     notifyMsg = "domained Script Finished for {}".format(domain)
     Config = configparser.ConfigParser()
     Config.read(os.path.join(script_path, "ext/notifycfg.ini"))
-    if (Config.get('Pushover', 'enable')) == "True":
-        poToken = (Config.get('Pushover', 'token'))
-        poUser = (Config.get('Pushover', 'user'))
-        if "device" in Config.options('Pushover'):
-            poDevice = (Config.get('Pushover', 'device'))
-            poRequestPayload = {'token': poToken, 'user': poUser, 'device': poDevice, 'title': notifySub,
-                                'message': notifyMsg}
+    if (Config.get("Pushover", "enable")) == "True":
+        poToken = Config.get("Pushover", "token")
+        poUser = Config.get("Pushover", "user")
+        if "device" in Config.options("Pushover"):
+            poDevice = Config.get("Pushover", "device")
+            poRequestPayload = {
+                "token": poToken,
+                "user": poUser,
+                "device": poDevice,
+                "title": notifySub,
+                "message": notifyMsg,
+            }
         else:
-            poRequestPayload = {'token': poToken, 'user': poUser, 'title': notifySub, 'message': notifyMsg}
-            poValidatePayload = {"token": poToken, 'user': poUser}
-            poValidate = requests.post('https://api.pushover.net/1/users/validate.json', data=(poValidatePayload))
+            poRequestPayload = {
+                "token": poToken,
+                "user": poUser,
+                "title": notifySub,
+                "message": notifyMsg,
+            }
+            poValidatePayload = {"token": poToken, "user": poUser}
+            poValidate = requests.post(
+                "https://api.pushover.net/1/users/validate.json",
+                data=(poValidatePayload),
+            )
             poJsonV = poValidate.json()
-            if poJsonV['status'] == 1:
+            if poJsonV["status"] == 1:
                 print("\nPushover Account Validated\n")
-                poRequest = requests.post('https://api.pushover.net/1/messages.json', data=(poRequestPayload))
+                poRequest = requests.post(
+                    "https://api.pushover.net/1/messages.json", data=(poRequestPayload)
+                )
                 poJsonR = poRequest.json()
-                if poJsonV['status'] == 1:
+                if poJsonV["status"] == 1:
                     print("\nPushover Account Notified\n")
                 else:
                     print("\nError - Pushover Account Not Notified\n")
             else:
                 print("\nError - Pushover Account Not Validated\n")
-    if (Config.get('Email', 'enable')) == "True":
-        gmailUser = (Config.get('Email', 'user'))
-        gmailPass = (Config.get('Email', 'password'))
+    if (Config.get("Email", "enable")) == "True":
+        gmailUser = Config.get("Email", "user")
+        gmailPass = Config.get("Email", "password")
         try:
-            server = smtplib.SMTP('smtp.gmail.com', 587)
+            server = smtplib.SMTP("smtp.gmail.com", 587)
             server.starttls()
             server.login(gmailUser, gmailPass)
             subject = "domained Script Complete"
-            text = ("domained Script Complete for " + domain)
-            msg = 'Subject: {}\n\n{}'.format(subject, text)
+            text = "domained Script Complete for " + domain
+            msg = "Subject: {}\n\n{}".format(subject, text)
             server.sendmail(gmailUser, gmailUser, msg)
             server.quit()
             print("\nEmail Notification Sent\n")
@@ -509,7 +554,7 @@ if __name__ == "__main__":
         vpncheck()
     if fresh:
         os.system("rm -r output")
-        newpath = r'output'
+        newpath = r"output"
         os.makedirs(newpath)
     if install:
         upgradeFiles()
